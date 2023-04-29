c6c3f9fbfe5b85a01eface5a1f9c6f168064b38f
Sam Jiang <129788706+SamJiang47@users.noreply.github.com>
2023-04-29 10:32:53 -0700

    Update main.js

--- SillyStoryGenerator/main.js
+++ SillyStoryGenerator/main.js
@@ -5,29 +5,29 @@ function randomValueFromArray(array){
                                      const random = Math.floor(Math.random()*array.length);
                                      return array[random];
                                     }
-let storyText= 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
-const insertX= ['Willy the Goblin','Big Daddy','Father Christmas'];
-const insertY= ['the soup kitchen','Disneyland','the White House'];
-const insertZ= ['spontaneously combusted','melted into a puddle on the sidewalk','turned into a slug and crawled away'];
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 randomize.addEventListener('click', result);
 function result() {
-                   if(customName.value !== '') {
-                                                const name = customName.value;
-                                                newStory = newStory.replaceAll('Bob',customName);
-                                               }
-                   if(document.getElementById("uk").checked) {
-                                                              const weight = Math.round(300*0.0714286)+'stone';
-                                                              newStory = newStory.replaceAll('300 pounds',weight);
-                                                              const temperature =  Math.round((94-32)*5/9)+'centigrade';
-                                                              newStory = newStory.replaceAll('94 fahrenheit',temperature);
-                                                             }
+                   let newStory = storyText;
+                   const xItem = randomValueFromArray(insertX);
+                   const yItem = randomValueFromArray(insertY);
+                   const zItem = randomValueFromArray(insertZ);
+                   newStory = newStory.replaceAll(':insertx:',xItem);
+                   newStory = newStory.replaceAll(':inserty:',yItem);
+                   newStory = newStory.replaceAll(':insertz:',zItem);
+                   if (customName.value !== '') {
+                                                 const name = customName.value;
+                                                 newStory = newStory.replaceAll('Bob', name);
+                                                }
+                   if (document.getElementById("uk").checked) {
+                                                               const weight = `${Math.round(300*0.0714286)} stone`;
+                                                               const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+                                                               newStory = newStory.replaceAll('94 fahrenheit', temperature);
+                                                               newStory = newStory.replaceAll('300 pounds', weight);
+                                                              }
                    story.textContent = newStory;
                    story.style.visibility = 'visible';
-                   const newStory= storyText;
-                   const xItem=randomValueFromArray(insertX);
-                   const yItem=randomValueFromArray(insertY);
-                   const zItem=randomValueFromArray(insertZ);
-                   newStory = newStory.replaceAll(':insertX:',xItem);
-                   newStory = newStory.replaceAll(':insertY:',yItem);
-                   newStory = newStory.replaceAll(':insertZ:',zItem);
-                  }
+                 }
